9d1839a0b9abc309e385b69d5d77d4efd7956981
Z4UR1X <100430181+Z4UR1X@users.noreply.github.com>
2023-11-14 17:28:02 -0300

    Main Files
    Added first version of the webpage.
    - Main code files: HTML, CSS, JavaScript.
    - Resources files: fonts and images.

---
+++ JS/dentcar-script.js
@@ -0,0 +1,83 @@
+/* Fade effect on images in home section. */
+const images = [
+    '/Resources/Images/DentCar-Gallery/bolloDemo1.jpg',
+    '/Resources/Images/DentCar-Gallery/bolloDemo2.jpg',
+    '/Resources/Images/DentCar-Gallery/bolloDemo3.jpg'
+  ];
+  
+  const displayImage = document.getElementById('display-image');
+  const imageContainer = document.getElementById('image-container');
+  
+  let currentIndex = 0;
+  
+  function showNextImage() {
+    displayImage.style.opacity = 0;
+    setTimeout(() => {
+      displayImage.src = images[currentIndex];
+      displayImage.style.opacity = 1;
+      currentIndex = (currentIndex + 1) % images.length;
+    }, 500);
+  }
+  
+  showNextImage();
+  setInterval(showNextImage, 5000);
+/* -x- */
+
+/* Transparent effect to NavBar brackground and his buttons. */
+const nav = document.querySelector('nav');
+const buttonNav = document.querySelectorAll('.nav-button');
+const navP = document.querySelectorAll('.nav-p');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  if (scrollY >= 0.18 * window.innerHeight) {
+    nav.style.backgroundColor = 'transparent';
+    nav.style.borderColor = 'transparent';
+    buttonNav.forEach(button => {
+      button.style.width = '150px';
+    });
+    navP.forEach(navParagraph => {
+      navParagraph.style.borderBottom = 'solid 2px var(--red-detail)';
+    });
+  } else {
+    nav.style.backgroundColor = 'var(--panel-color)';
+    nav.style.borderColor = 'red';
+    buttonNav.forEach(button => {
+      button.style.width = '';
+    });
+    navP.forEach(navParagraph => {
+      navParagraph.style.borderBottom = 'transparent';
+    });
+  }
+});
+/* -x- */
+
+/* Main title movement and fadeout effect */
+const titleRightAnim = document.querySelectorAll('.anim-mov-right');
+const titleLeftAnim = document.querySelectorAll('.anim-mov-left');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  titleRightAnim.forEach(element => {
+    if (scrollY >= 0.20 * window.innerHeight) {
+      element.style.transform = 'translateX(100%)';
+      element.style.opacity = '0';
+    } else {
+      element.style.transform = 'translateX(0%)';
+      element.style.opacity = '1';
+    }
+  });
+
+  titleLeftAnim.forEach(element => {
+    if (scrollY >= 0.20 * window.innerHeight) {
+      element.style.transform = 'translateX(-100%)';
+      element.style.opacity = '0';
+    } else {
+      element.style.transform = 'translateX(0%)';
+      element.style.opacity = '1';
+    }
+  });
+});
+/* -x- */
